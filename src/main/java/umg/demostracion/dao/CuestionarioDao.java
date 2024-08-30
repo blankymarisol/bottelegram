36738c6d6c2e599f3c52024d3e0b2063289ee1df
blankymarisol <blopezm50@miumg.edu.gt>
2024-08-29 18:59:40 -0600

    tarea no terminada

---
+++ src/main/java/umg/demostracion/dao/CuestionarioDao.java
@@ -0,0 +1,24 @@
+package umg.demostracion.dao;
+
+
+import umg.demostracion.db.DatabaseConnection;
+import umg.demostracion.model.Cuestionario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CuestionarioDao {
+    public void insertUser(Cuestionario cuestionario) throws SQLException {
+        String query = "INSERT INTO tb_respuestas(seccion,telegram_id,respuesta_texto,pregunta_id) VALUES (?,?,?,?)";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, cuestionario.getSeccion());
+            statement.setLong(2, cuestionario.getTelegramid());
+            statement.setString(3, cuestionario.getResponse());
+            statement.setInt(4, cuestionario.getPreguntaid());
+
+            statement.executeUpdate();
+        }
+    }
+}
---
+++ src/main/java/umg/demostracion/service/CuestionarioService.java
@@ -0,0 +1,28 @@
+package umg.demostracion.service;
+
+
+import umg.demostracion.dao.CuestionarioDao;
+import umg.demostracion.db.DatabaseConnection;
+import umg.demostracion.db.TransactionManager;
+import umg.demostracion.model.Cuestionario;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CuestionarioService {
+    private CuestionarioDao cuestionarioDao = new CuestionarioDao();
+
+    public void crearUsuario(Cuestionario cuestionario) throws SQLException {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            TransactionManager tm = new TransactionManager(connection);
+            tm.beginTransaction();
+            try {
+                cuestionarioDao.insertUser(cuestionario);
+                tm.commit();
+            } catch (SQLException e) {
+                tm.rollback();
+                throw e;
+            }
+        }
+    }
+}
